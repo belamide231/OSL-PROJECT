0d8160eefe0bdf72632ce29086348f2ae78571fb
Belamide Mills B <belamidemills29@gmail.com>
2025-02-19 20:55:08 +0800

    .

--- back/src/configuration/redis.ts
+++ back/src/configuration/redis.ts
@@ -1,5 +1,8 @@
 import { createClient, RedisClientType } from 'redis';
 import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+
 
 dotenv.config();
 
@@ -18,10 +21,23 @@ export class getRedisConnection {
     }
 
     private async initialize() {
+
         try {
 
+            const scriptPath = path.join(__dirname, '../../scripts');
+            const getMessageScript = fs.readFileSync(path.join(scriptPath, 'getMessages.lua'), 'utf-8') as string;
+
             await this.con.connect();
+            await this.con.sendCommand(['FUNCTION', 'LOAD', 'REPLACE', getMessageScript]);
+
+            const result = await this.con.sendCommand(['FCALL', 'getMessages', '0', '1', '3']) as any;
+            const found = result[0];
+            const messages = result[1].map((x: string) => JSON.parse(x));
+
+            console.log(messages);
+
             console.log(`CONNECTED TO REDIS ${process.env.CLOUD_BASE ? 'CLOUD' : 'LOCAL'}`);
+
         } catch (error) {
 
             console.log("REDIS ERROR");
